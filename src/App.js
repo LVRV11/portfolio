4a5f860b89c9ab5c844c29e52e4466a7c9097848
Dmitrii Lavrov <lavrovdmitrii@gmail.com>
2023-05-03 01:17:40 +0200

    normal

--- src/App.js
+++ src/App.js
@@ -6,24 +6,23 @@ import Contacts from './pages/Contacts';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './utils/scrollToTop';
 
 
-
-
-// import React from 'react';
 
 function App() {
 	return (
     <div className="App">
         <Router>
+          <ScrollToTop />
 		        <Navbar />
-            <Routes>
+              <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/projects' element={<Projects />} />
               <Route path='/contacts' element={<Contacts />} />
               <Route path='/projectAdd' element={<ProjectAdd />} />
-            </Routes>
-            <Footer />
+          </Routes>
+          <Footer />
         </Router>
 	  </div>
   );
