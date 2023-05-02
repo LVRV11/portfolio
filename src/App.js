b4f81c4ed5d1d6597f87ac105eaa1d40c175d001
Dmitrii Lavrov <lavrovdmitrii@gmail.com>
2023-05-02 17:49:46 +0200

    4t

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
-import './styles/main.css'
-import './styles/reset.css'
+import './styles/main.css';
+import Navbar from './components/navbar/Navbar';
+import Header from './components/header/Header';
+import vk from './img/icons/vk.svg';
+import twitter from './img/icons/twitter.svg';
+import linkedIn from './img/icons/linkedIn.svg';
+import instagram from './img/icons/instagram.svg';
+import gitHub from './img/icons/gitHub.svg';
 
-import React from 'react';
+
+// import React from 'react';
 
 function App() {
-	return <div>
-		<h1>HELLO g</h1>
-	</div>; 
+	return (
+    <div className="App">
+		    <Navbar />
+        <Header />
+
+        
+
+        <main className="section">
+        <div className="container">
+
+                <ul className="content-list">
+                    <li className="content-list__item">
+                        <h2 className="title-2">Frontend</h2>
+                        <p>JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents</p>
+                    </li>
+                    <li className="content-list__item">
+                        <h2 className="title-2">Backend</h2>
+                        <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
+                    </li>
+                </ul>
+
+        </div>
+        </main>
+
+        <footer className="footer">
+        <div className="container">
+            <div className="footer__wrapper">
+                <ul className="social">
+                    <li className="social__item"><a href="#!"><img src={vk} alt="Link" /></a></li>
+                    <li className="social__item"><a href="#!"><img src={instagram} alt="Link" /></a></li>
+                    <li className="social__item"><a href="#!"><img src={twitter} alt="Link" /></a></li>
+                    <li className="social__item"><a href="#!"><img src={linkedIn} alt="Link" /></a></li>
+                    <li className="social__item"><a href="#!"><img src={gitHub} alt="Link" /></a></li>
+                </ul>
+                <div className="copyright">
+                    <p>© 2022 frontend-dev.com</p>
+                </div>
+            </div>
+        </div>
+        </footer> 
+	</div>
+  );
 }
 
 export default App;
